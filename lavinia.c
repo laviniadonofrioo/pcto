bbe476498bcae1386958a99d4367b24431cf3b13
laviniadonofrioo <127745446+laviniadonofrioo@users.noreply.github.com>
2023-03-13 16:48:13 +0100

    Add files via upload

---
+++ lavinia.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main ()
+{
+    int x;
+    int y;
+    printf("ínserisci un numero : ");
+    scanf("%d", &x);
+    printf("ínserisci un altro numero:");
+    scanf("%d", &y);
+    printf("il numero inserito è %d\n", x);
+    printf("i numeri inseriti sono x = %d e y = %d\n", x, y);
+}
