562cdd1e37c5939f70ff1b2f556256b3bfcd74fe
laviniadonofrioo <127745446+laviniadonofrioo@users.noreply.github.com>
2023-03-13 15:45:53 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero = 42;
+    double numerolungo = 544475477;
+    float numeroconlavirgola = 5.676;
+    char carattere = 99;
+    printf ("il numero è: %d\n", numero);
+    printf ("numero lungo è: %f, il numero con la virgola e %f\n", numerolungo, numeroconlavirgola);
+} 
