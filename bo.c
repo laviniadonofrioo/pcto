d71ac0f3ef78f59212b805251851949bef179549
laviniadonofrioo <127745446+laviniadonofrioo@users.noreply.github.com>
2023-03-14 11:21:58 +0100

    Add files via upload

---
+++ bo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int eta;
+    printf ("la mia eta è: ");
+    scanf ("%d", &eta);
+    if (eta < 18)
+    {
+    printf("sono minorenne\n");
+    }   
+    else
+    {
+    printf("sono maggiorenne\n");
+    }
+}
+
+
